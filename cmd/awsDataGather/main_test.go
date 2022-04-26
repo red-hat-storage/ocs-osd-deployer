eca2091d7bfbfd24116bba6060e01c8069ff2f55
Renan Campos <rcampos@redhat.com>
2022-04-26 18:38:44 -0400

    Using AWS data for network policy setup.
    Provider clusters run storage pods on the host network namespace of
    their worker nodes. To properly set up the network policy, the machine
    CIDR must be known. In AWS, this can be determined by making a call to
    the IMDSv2 server that is available in any VPC. This PR creates a
    deployment to gather the needed data, and modifies the reconcilation
    loop to make use of the AWS data when setting up the network policies.
    
    Signed-off-by: Renan Campos <rcampos@redhat.com>

---
+++ cmd/awsDataGather/main_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	tutils "github.com/red-hat-storage/ocs-osd-deployer/testutils"
+	"github.com/red-hat-storage/ocs-osd-deployer/utils"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+var _ = Describe("AWS Data Gathering behavior", func() {
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+	ctx := context.Background()
+
+	configMap := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      utils.IMDSConfigMapName,
+			Namespace: testNamespace,
+		},
+	}
+
+	fakeDeployment := appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "fake-deployment",
+			Namespace: testNamespace,
+			UID:       "foobar",
+		},
+	}
+
+	When("aws data gathering successfully completes", func() {
+		var awsConfigMap corev1.ConfigMap
+
+		BeforeEach(func() {
+			err := gatherAndSaveData("http://"+testMockerAddr, fakeDeployment, k8sClient, context.TODO())
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should have created a configmap that is valid...", func() {
+			err := k8sClient.Get(ctx, tutils.GetResourceKey(&configMap), &awsConfigMap)
+			Expect(err).ToNot(HaveOccurred())
+
+			By("having the VPC cidr key")
+			vpcCIDR, ok := awsConfigMap.Data[utils.CIDRKey]
+			Expect(ok).To(Equal(true))
+			Expect(vpcCIDR).To(Equal(fakeCIDR))
+
+			By("having a deployment owner reference")
+			Expect(len(awsConfigMap.OwnerReferences)).To(Equal(1))
+		})
+
+		AfterEach(func() {
+			err := k8sClient.Delete(ctx, &awsConfigMap)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+
+	When("The configmap aws-data already exists", func() {
+		BeforeEach(func() {
+			err := k8sClient.Create(ctx, &configMap)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		When("aws data gathering runs", func() {
+			BeforeEach(func() {
+				err := gatherAndSaveData("http://"+testMockerAddr, fakeDeployment, k8sClient, context.TODO())
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("should have updated the configmap", func() {
+				err := k8sClient.Get(ctx, tutils.GetResourceKey(&configMap), &configMap)
+				Expect(err).ToNot(HaveOccurred())
+
+				By("adding VPC cidr key")
+				vpcCIDR, ok := configMap.Data[utils.CIDRKey]
+				Expect(ok).To(Equal(true))
+				Expect(vpcCIDR).To(Equal(fakeCIDR))
+
+				By("adding a deployment owner reference")
+				Expect(len(configMap.OwnerReferences)).To(Equal(1))
+			})
+		})
+
+		AfterEach(func() {
+			err := k8sClient.Delete(ctx, &configMap)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
